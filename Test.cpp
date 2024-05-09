2c094548ac37425a10c7204e9f6043deb7f038d0
eilon315233 <156177204+eilon315233@users.noreply.github.com>
2024-05-09 23:59:43 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,10 +1,12 @@
 // ID: 315233486
 // Email: eilonashwal30@gmail.com
 
+#include <iostream>
+#include <vector>
+
 #include "doctest.h"
 #include "Algorithms.hpp"
 #include "Graph.hpp"
-#include <stdexcept>
 using namespace std;
 
 TEST_CASE("Test isConnected")
@@ -28,7 +30,7 @@ TEST_CASE("Test isConnected")
     CHECK(ariel::Algorithms::isConnected(grp) == false);
 }
 
-TEST_CASE("Test shortestPath")
+TEST_CASE("Test ShortestPath")
 {
     ariel::Graph grp;
     vector<vector<int>> graph = {
@@ -338,6 +340,221 @@ TEST_CASE("Test negativeCycle with no negative cycle") {
         {0, 1, 0}};
     g.loadGraph(graph);
     CHECK(ariel::Algorithms::negativeCycle(g) == "the graph does not contain a negative cycle");
+}
+
+TEST_CASE("Test isConnected")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isConnected(g) == true);
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::isConnected(g) == false);
+
+    vector<vector<int>> graph3 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph3);
+    CHECK(ariel::Algorithms::isConnected(g) == true);
+
+    vector<vector<int>> graph4 = {
+        {0, 1, 0},
+        {1, 0, 0},
+        {0, 0, 0}};
+    g.loadGraph(graph4);
+    CHECK(ariel::Algorithms::isConnected(g) == false);
+
+    vector<vector<int>> graph5 = {
+        {0, 1, 0},
+        {0, 0, 1},
+        {1, 0, 0}};
+    g.loadGraph(graph5);
+    CHECK(ariel::Algorithms::isConnected(g) == true);
+}
+
+TEST_CASE("Test shortestPath_2")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 2) == "0->1->2");
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 4) == "-1");
+
+    vector<vector<int>> graph3 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph3);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 2) == "0->1->2");
+
+    vector<vector<int>> graph4 = {
+        {0, 1, 0},
+        {1, 0, 0},
+        {0, 0, 0}};
+    g.loadGraph(graph4);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 2) == "-1");
+
+    vector<vector<int>> graph6 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph6);
+    CHECK(ariel::Algorithms::shortestPath(g, 3, 2) == "-1");
+
+}
+
+TEST_CASE("Test isContainsCycle")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == false);
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == true);
+
+    vector<vector<int>> graph3 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph3);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == false);
+
+    vector<vector<int>> graph4 = {
+        {0, 1, 0},
+        {1, 0, 0},
+        {0, 0, 0}};
+    g.loadGraph(graph4);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == false);
+
+    vector<vector<int>> graph5 = {
+        {0, 1, 0},
+        {0, 0, 1},
+        {1, 0, 0}};
+    g.loadGraph(graph5);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == true);
+
+    vector<vector<int>> graph6 = {
+        {0, 1, 0},
+        {0, 0, 0},
+        {1, 0, 0}};
+    g.loadGraph(graph6);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == false);
+
+    vector<vector<int>> graph7 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph7);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == false);
+
+    vector<vector<int>> graph8 = {
+        {0, 1, 1},
+        {1, 0, 1},
+        {1, 1, 0}};
+    g.loadGraph(graph8);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == true);
+}
+
+TEST_CASE("Test isBipartite")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isBipartite(g) == "The graph is bipartite: A={0, 2}, B={1}");
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::isBipartite(g) == "0");
+
+    vector<vector<int>> graph3 = {
+        {0, 1, 0, 0, 0},
+        {1, 0, 3, 0, 0},
+        {0, 3, 0, 4, 0},
+        {0, 0, 4, 0, 5},
+        {0, 0, 0, 5, 0}};
+    g.loadGraph(graph3);
+    CHECK(ariel::Algorithms::isBipartite(g) == "The graph is bipartite: A={0, 2, 4}, B={1, 3}");
+
+    vector<vector<int>> graph4 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph4);
+    CHECK(ariel::Algorithms::isBipartite(g) == "The graph is bipartite: A={0, 2}, B={1}");
+
+    vector<vector<int>> graph5 = {
+        {0, 1, 1},
+        {1, 0, 1},
+        {1, 1, 0}};
+    g.loadGraph(graph5);
+    CHECK(ariel::Algorithms::isBipartite(g) == "0");    
+}
+
+TEST_CASE("Test invalid graph")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 2, 0},
+        {1, 0, 3, 0},
+        {2, 3, 0, 4},
+        {0, 0, 4, 0},
+        {0, 0, 0, 5}};
+    CHECK_THROWS(g.loadGraph(graph));
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 2},
+        {1, 0, 3},
+        {2, 3, 0},
+        {0, 0, 4},
+        {0, 0, 5}};
+    CHECK_THROWS(g.loadGraph(graph2));
+
+    vector<vector<int>> graph3 = {
+        {0, 1, 2, 2, 2, 2, 2},
+        {2, 3, 4, 5, 6, 0, 7},
+        {2, 3, 4, 5, 6, 7, 0}};
+    CHECK_THROWS(g.loadGraph(graph3));
+
 }
 
 
